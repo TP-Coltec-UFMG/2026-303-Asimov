48d71c220a9e10c3bdaa00bef361c34c53923b78
Gab20255 <gabrielodb23@gmail.com>
2026-09-02 21:40:25 -0300

    Delete Objects directory

--- Objects/scripts/arma.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-@onready var sprite_2d: Sprite2D = $Sprite2D
-
-var player: Player = null
-
-
-func set_player(novo_player: Player) -> void:
-	player = novo_player
--- Objects/scripts/conhecimento.gd
+++
@@ -1,3 +0,0 @@
-extends Node2D
-
-@onready var sprite_2d: Sprite2D = $Sprite2D
